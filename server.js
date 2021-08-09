b25caef240ebbbf7eee9cf853a86c4184f021436
krishna.chavan <krishna.chavan@brillio.com>
2021-08-09 10:25:32 +0530

    create order

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
 const Product = mongoose.model(
     "products",
     new mongoose.Schema({
-        _id: { type: String, default:shortid.generate },
+        _id: { type: String, default: shortid.generate },
         title: String,
         description: String,
         image: String,
         price: Number,
-        availableSizes:[String]
+        availableSizes: [String]
     })
 );
 
@@ -29,16 +29,48 @@ app.get("/api/products", async (req, res) => {
     res.send(products);
 });
 
-app.post("/api/products", async (req,res) => {
+app.post("/api/products", async (req, res) => {
     const newProduct = new Product(req.body);
     const saveProduct = await newProduct.save();
     res.send(saveProduct);
 });
 
-app.delete("/api/products/:id", async (req,res)=>{
+app.delete("/api/products/:id", async (req, res) => {
     const deletedProduct = await Product.findByIdAndDelete(req.params.id);
     res.send(deletedProduct);
 });
 
+const Order = mongoose.model("order", new mongoose.Schema({
+    _id: {
+        type: String,
+        default: shortid.generate
+    },
+    email: String,
+    name: String,
+    address: String,
+    total: Number,
+    cartItems: [{
+        _id: String,
+        title: String,
+        price: Number,
+        count: Number,
+    }]
+},
+    {
+        timestamps: true
+    }));
+    app.post("/api/orders", async (req, res) => {
+        if(!req.body.name ||
+                    !req.body.email ||
+                    !req.body.address ||
+                    !req.body.total ||
+                    !req.body.cartItems 
+                    ) {
+                        return res.send({message:"data is required."})
+            }
+            const order = await Order(req.body).save();
+            res.send(order);
+    });
+
 const port = process.env.PORT || 5000;
-app.listen(port, ()=> console.log("serve at http://localhost:5000"))
+app.listen(port, () => console.log("serve at http://localhost:5000"))
--- src/store.js
+++ src/store.js
@@ -1,16 +1,19 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import { cartReducer } from "./reducers/cartReducer";
 import { productsReducers } from "./reducers/productReducers";
+// import { cartReducer } from "./reducers/cartReducers";
+import { orderReducer } from "./reducers/orderReducers";
+import { cartReducer } from "./reducers/cartReducer";
 
 const initialState = {};
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-    combineReducers({
-        products: productsReducers,
-        cart: cartReducer,
-    }),
-    initialState,
-    composeEnhancer(applyMiddleware(thunk))
+  combineReducers({
+    products: productsReducers,
+    cart: cartReducer,
+    order: orderReducer,
+  }),
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
 );
 export default store;
